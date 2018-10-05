ea68858d1fa60aaf22346e1b056afdb0e7750d50
Dylan Chapman <dylanchapmanweb@gmail.com>
2018-10-04 19:02:26 -0500

    Added AllHeroesSettings view

---
+++ src/views/AllHeroesSettings.vue
@@ -0,0 +1,16 @@
+<script>
+import allHeroesSettings from "@/data/default-settings/all-heroes";
+import Settings from "@/components/Settings";
+
+export default {
+	name: "AllHeroesSettings",
+
+	render() {
+		return <Settings
+			banner={require( "@/assets/img/banners/all-heroes.png" )}
+			settings={allHeroesSettings}
+			title={"All Heroes Settings"}
+		></Settings>;
+	}
+};
+</script>
