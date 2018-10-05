35e5296ff5e92b282b27dbd202945405afd3580d
Dylan Chapman <dylanchapmanweb@gmail.com>
2018-10-04 20:19:31 -0500

    Added banners for all heroes

--- src/data/heroes.js
+++ src/data/heroes.js
@@ -9,162 +9,189 @@ export default [
 	{
 		name: "Bastion",
 		key: "bastion",
+		banner: require( "@/assets/img/banners/bastion.jpg" ),
 		image: require( "@/assets/img/heroes/bastion.png" )
 	},
 
 	{
 		name: "Brigitte",
 		key: "brigitte",
+		banner: require( "@/assets/img/banners/brigitte.jpg" ),
 		image: require( "@/assets/img/heroes/brigitte.png" )
 	},
 
 	{
 		name: "D.Va",
 		key: "dva",
+		banner: require( "@/assets/img/banners/dva.jpg" ),
 		image: require( "@/assets/img/heroes/dva.png" )
 	},
 
 	{
 		name: "Doomfist",
 		key: "doomfist",
+		banner: require( "@/assets/img/banners/doomfist.jpg" ),
 		image: require( "@/assets/img/heroes/doomfist.png" )
 	},
 
 	{
 		name: "Genji",
 		key: "genji",
+		banner: require( "@/assets/img/banners/genji.jpg" ),
 		image: require( "@/assets/img/heroes/genji.png" )
 	},
 
 	{
 		name: "Hanzo",
 		key: "hanzo",
+		banner: require( "@/assets/img/banners/hanzo.jpg" ),
 		image: require( "@/assets/img/heroes/hanzo.png" )
 	},
 
 	{
 		name: "Junkrat",
 		key: "junkrat",
+		banner: require( "@/assets/img/banners/junkrat.jpg" ),
 		image: require( "@/assets/img/heroes/junkrat.png" )
 	},
 
 	{
 		name: "Lúcio",
 		key: "lucio",
+		banner: require( "@/assets/img/banners/lucio.jpg" ),
 		image: require( "@/assets/img/heroes/lucio.png" )
 	},
 
 	{
 		name: "McCree",
 		key: "mccree",
+		banner: require( "@/assets/img/banners/mccree.jpg" ),
 		image: require( "@/assets/img/heroes/mccree.png" )
 	},
 
 	{
 		name: "Mei",
 		key: "mei",
+		banner: require( "@/assets/img/banners/mei.jpg" ),
 		image: require( "@/assets/img/heroes/mei.png" )
 	},
 
 	{
 		name: "Mercy",
 		key: "mercy",
+		banner: require( "@/assets/img/banners/mercy.jpg" ),
 		image: require( "@/assets/img/heroes/mercy.png" )
 	},
 
 	{
 		name: "Moira",
 		key: "moira",
+		banner: require( "@/assets/img/banners/moira.jpg" ),
 		image: require( "@/assets/img/heroes/moira.png" )
 	},
 
 	{
 		name: "Orisa",
 		key: "orisa",
+		banner: require( "@/assets/img/banners/orisa.jpg" ),
 		image: require( "@/assets/img/heroes/orisa.png" )
 	},
 
 	{
 		name: "Pharah",
 		key: "pharah",
+		banner: require( "@/assets/img/banners/pharah.jpg" ),
 		image: require( "@/assets/img/heroes/pharah.png" )
 	},
 
 	{
 		name: "Reaper",
 		key: "reaper",
+		banner: require( "@/assets/img/banners/reaper.jpg" ),
 		image: require( "@/assets/img/heroes/reaper.png" )
 	},
 
 	{
 		name: "Reinhardt",
 		key: "reinhardt",
+		banner: require( "@/assets/img/banners/reinhardt.jpg" ),
 		image: require( "@/assets/img/heroes/reinhardt.png" )
 	},
 
 	{
 		name: "Roadhog",
 		key: "roadhog",
+		banner: require( "@/assets/img/banners/roadhog.jpg" ),
 		image: require( "@/assets/img/heroes/roadhog.png" )
 	},
 
 	{
 		name: "Soldier: 76",
 		key: "soldier-76",
+		banner: require( "@/assets/img/banners/soldier-76.jpg" ),
 		image: require( "@/assets/img/heroes/soldier-76.png" )
 	},
 
 	{
 		name: "Sombra",
 		key: "sombra",
+		banner: require( "@/assets/img/banners/sombra.jpg" ),
 		image: require( "@/assets/img/heroes/sombra.png" )
 	},
 
 	{
 		name: "Symmetra",
 		key: "symmetra",
+		banner: require( "@/assets/img/banners/symmetra.jpg" ),
 		image: require( "@/assets/img/heroes/symmetra.png" )
 	},
 
 	{
 		name: "Torbjörn",
 		key: "torbjorn",
+		banner: require( "@/assets/img/banners/torbjorn.jpg" ),
 		image: require( "@/assets/img/heroes/torbjorn.png" )
 	},
 
 	{
 		name: "Tracer",
 		key: "tracer",
+		banner: require( "@/assets/img/banners/tracer.jpg" ),
 		image: require( "@/assets/img/heroes/tracer.png" )
 	},
 
 	{
 		name: "Widowmaker",
 		key: "widowmaker",
+		banner: require( "@/assets/img/banners/widowmaker.jpg" ),
 		image: require( "@/assets/img/heroes/widowmaker.png" )
 	},
 
 	{
 		name: "Winston",
 		key: "winston",
+		banner: require( "@/assets/img/banners/winston.jpg" ),
 		image: require( "@/assets/img/heroes/winston.png" )
 	},
 
 	{
 		name: "Wrecking Ball",
 		key: "wrecking-ball",
+		banner: require( "@/assets/img/banners/wrecking-ball.jpg" ),
 		image: require( "@/assets/img/heroes/wrecking-ball.png" )
 	},
 
 	{
 		name: "Zarya",
 		key: "zarya",
+		banner: require( "@/assets/img/banners/zarya.jpg" ),
 		image: require( "@/assets/img/heroes/zarya.png" )
 	},
 
 	{
 		name: "Zenyatta",
 		key: "zenyatta",
+		banner: require( "@/assets/img/banners/zenyatta.jpg" ),
 		image: require( "@/assets/img/heroes/zenyatta.png" )
 	}
 ];
