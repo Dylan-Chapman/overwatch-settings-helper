55147430601726c353c0122b7d6c2fbc2f688d59
Dylan Chapman <dylanchapmanweb@gmail.com>
2018-11-01 23:33:57 -0500

    Integrated firebase/vuex into main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
-import "@babel/polyfill";
-import Vue from "vue";
 import "./plugins/vuetify";
-import App from "./App.vue";
 import "./registerServiceWorker";
+import "@babel/polyfill";
+import "firebase/auth";
+import App from "./App.vue";
+import Vue from "vue";
+import VueFirestore from "vue-firestore";
+import firebaseApp from "@/firebase/app";
 import router from "./router";
+import store from "./store";
+
+Vue.use( VueFirestore );
 
 Vue.config.productionTip = false;
 
-new Vue( {
-	router,
-	render: h => h( App )
-} ).$mount( "#app" );
+const unsubscribe = firebaseApp.auth().onAuthStateChanged( () => {
+	new Vue( {
+		router,
+		store,
+		render: h => h( App )
+	} ).$mount( "#app" );
+
+	unsubscribe();
+} );
+
